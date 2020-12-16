9af35b74bba7d65d34e9d6b3bf020b6c6f58d222
Mohamed187-ai <mohassan787898@gmail.com>
2020-12-16 22:36:57 +0200

    Last Commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Response;
+
+
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+           return  Response::json([
+                'error' => [
+                    'message' => 'Not Found!'
+                ]
+            ], 404);
+        }
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return  Response::json([
+                 'error' => [
+                     'message' => 'Not Supported for this route!'
+                 ]
+             ], 404);
+         }
+         /*else{
+            return  Response::json([
+                'error' => [
+                    'message' => 'Not Supported!'
+                ]
+            ], 404);
+         }*/
+        return parent::render($request, $exception);
+    }
+}
---
+++ database/seeders/LessonSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Lesson;
+
+class LessonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Lesson::factory()->times(100)->create();
+    }
+}
